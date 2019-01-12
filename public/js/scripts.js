91a78f9f771194b2c552c079ac0d128f7d04ee0f
SEAjamieD <jamie.degnan@gmail.com>
2019-01-11 23:29:27 -0800

    Add basic scaffolding of video and begin building functions

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,27 @@
 console.log('connected');
+
+let options = {
+  id: 59777392,
+  width: 460,
+  loop: false
+};
+
+const player = new Vimeo.Player('vimeo-slot', options);
+
+player.on('play', () => {
+  console.log('video playing');
+});
+
+player.on('pause', () => {
+  console.log('paused');
+});
+
+
+player.on('ended', () => {
+  console.log('video ended');
+});
+
+player.on('timeupdate', (data) => {
+  let percent = `${Math.round(data.percent * 100)}%`;
+  console.log(percent);
+})
