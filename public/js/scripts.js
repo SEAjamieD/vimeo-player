e5ab33ab07db96f86c2d0bf339eb5df65283bca5
SEAjamieD <jamie.degnan@gmail.com>
2019-01-12 08:36:14 -0800

    Set cookie expiration to 14 days, refactor getting cookie value into a function

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,5 @@
-// alert(document.cookie);
-window.jVimData = {};
+alert(document.cookie);
+JvimData = {};
 
 let options = {
   id: 59777392,
@@ -9,34 +9,17 @@ let options = {
 
 const player = new Vimeo.Player('vimeo-slot', options);
 
-player.on('play', () => {
-  console.log('video playing');
-});
-
-player.on('pause', () => {
-  console.log('paused');
-});
-
 player.on('timeupdate', (data) => {
-  jVimData.position = `${Math.round(data.percent * 100)}`; //store percentage in window object
-  jVimData.seconds = data.seconds;
+  JvimData.position = `${Math.round(data.percent * 100)}`; //store percentage in window object
+  JvimData.seconds = data.seconds;
 })
 
 player.on('ended', () => {
-  console.log('video ended');
+  console.log('video ended - update this to an alert of some sort');
 });
 
-//Listening for the browser window closing
-window.addEventListener('beforeunload', (e) => {
-  e.preventDefault();
-  document.cookie = `jVimSeconds=${jVimData.seconds}`;
-});
-
-//Check for existing time set cookie and set the player to that time
-if ( document.cookie.split(';').filter((item) => item.includes('jVimSeconds=')).length ) {
-  let seconds = document.cookie.replace(/(?:(?:^|.*;\s*)jVimSeconds\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-
-  player.setCurrentTime(`${seconds}`).then( (s) => {
+const setVimeoPosition = (seconds) => {
+  player.setCurrentTime(seconds).then( (s) => {
 
   }).catch( (error) => {
     switch (error.name) {
@@ -44,9 +27,29 @@ if ( document.cookie.split(';').filter((item) => item.includes('jVimSeconds=')).
       break;
 
       default:
-      console.error(error);
+      // console.error(error);
       break;
     }
   });
+}
 
+//Listening for the browser window closing and set cookie 14 expiring in 14 days
+window.addEventListener('beforeunload', (e) => {
+  e.preventDefault();
+  let today = new Date();
+  let twoWeeks = new Date(Date.now() + 12096e5);
+  twoWeeks = twoWeeks.toUTCString();
+  document.cookie = `jVimSeconds=${JvimData.seconds}; expires=${twoWeeks}`;
+});
+
+//Check for existing time set cookie and set the player to that time
+const getCookieSeconds = () => {
+
+  if ( document.cookie.split(';').filter((item) => item.includes('jVimSeconds=')).length ) {
+    let seconds = document.cookie.replace(/(?:(?:^|.*;\s*)jVimSeconds\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    seconds !== undefined
+    ? setVimeoPosition(seconds)
+    : '';
+  }
+  
 }
