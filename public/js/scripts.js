b97342608bd289db049a651a4762a91622425784
SEAjamieD <jamie.degnan@gmail.com>
2019-01-12 00:36:16 -0800

    Storing the seconds watched in a cookie and setting the player to move to that time upon refresh, currently using default expiration

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,4 @@
-console.log('connected');
+// alert(document.cookie);
 window.jVimData = {};
 
 let options = {
@@ -18,24 +18,35 @@ player.on('pause', () => {
 });
 
 player.on('timeupdate', (data) => {
-  let percent = `${Math.round(data.percent * 100)}`;
-  jVimData.position = percent; //store percentage in window object
-  console.log(percent);
+  jVimData.position = `${Math.round(data.percent * 100)}`; //store percentage in window object
+  jVimData.seconds = data.seconds;
 })
 
 player.on('ended', () => {
   console.log('video ended');
 });
 
-
 //Listening for the browser window closing
 window.addEventListener('beforeunload', (e) => {
   e.preventDefault();
-  document.cookie = `jVimValue=${jVimData.position}`;
+  document.cookie = `jVimSeconds=${jVimData.seconds}`;
 });
 
-//Check for existing cookie
-if ( document.cookie.split(';').filter((item) => item.includes('jVimValue=')).length ) {
-  // var cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)testing\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-  alert(document.cookie);
+//Check for existing time set cookie and set the player to that time
+if ( document.cookie.split(';').filter((item) => item.includes('jVimSeconds=')).length ) {
+  let seconds = document.cookie.replace(/(?:(?:^|.*;\s*)jVimSeconds\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+
+  player.setCurrentTime(`${seconds}`).then( (s) => {
+
+  }).catch( (error) => {
+    switch (error.name) {
+      case 'RangeError':
+      break;
+
+      default:
+      console.error(error);
+      break;
+    }
+  });
+
 }
