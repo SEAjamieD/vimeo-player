f8cf41bbeb16167a50bdf0c3313e1337be2806e0
SEAjamieD <jamie.degnan@gmail.com>
2019-01-11 23:41:23 -0800

    Wiring up basic implementation of cookies

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,5 @@
 console.log('connected');
+alert(document.cookie);
 
 let options = {
   id: 59777392,
@@ -16,12 +17,18 @@ player.on('pause', () => {
   console.log('paused');
 });
 
+player.on('timeupdate', (data) => {
+  let percent = `${Math.round(data.percent * 100)}%`;
+  console.log(percent);
+})
 
 player.on('ended', () => {
   console.log('video ended');
 });
 
-player.on('timeupdate', (data) => {
-  let percent = `${Math.round(data.percent * 100)}%`;
-  console.log(percent);
-})
+
+//Listening for the browser window closing
+window.addEventListener('beforeunload', (e) => {
+  e.preventDefault();
+  document.cookie = "testing=test_cookie";
+});
