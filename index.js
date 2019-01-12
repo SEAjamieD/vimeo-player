168615acaeb1effd20e3ae673e0258c94260e24d
SEAjamieD <jamie.degnan@gmail.com>
2019-01-11 20:08:44 -0800

    Formatting and remove unneccesary export from index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -10,4 +10,3 @@ app.get('/', (req, res) => {
 
 app.listen('5000');
 console.log('Listening on 5000');
-exports = module.exports = app;
